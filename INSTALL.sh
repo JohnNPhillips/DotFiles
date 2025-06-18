f0ce506c24f72e403d266dd61c0dd525a0ad0e99
John Phillips <JohnPhillips861@gmail.com>
2025-06-18 15:08:50 -0700

    More robust INSTALL script
    Also, automatically add mybashrc source line to .bashrc

--- INSTALL.sh
+++ INSTALL.sh
@@ -8,8 +8,27 @@ then
 	exit 1
 fi
 
-mv ~/.mybashrc ~/.mybashrc.bak
-mv ~/.vimrc ~/.vimrc.bak
-ln -s $DIR/mybashrc ~/.mybashrc
-ln -s $DIR/vimrc ~/.vimrc
+DOTFILES=(mybashrc vimrc)
 
+for file in ${DOTFILES[@]}; do
+    # If the file exists, back it up
+    if [ -f ~/.$file ]; then
+        if [ ! -f ~/.$file.bak ]; then
+            echo "Backing up ~/.$file to ~/.$file.bak"
+            mv ~/.$file ~/.$file.bak
+        fi
+    fi
+
+    # If the symlink doesn't exist, create it
+    if [ ! -h ~/.$file ]; then
+        echo "Creating symlink for $file"
+        ln -s $DIR/$file ~/.$file
+    fi
+done
+
+# Special handling for .bashrc
+BASHRC_SNIPPET="source ~/.mybashrc"
+if [ -f ~/.bashrc ] && ! grep -qF "$BASHRC_SNIPPET" ~/.bashrc; then
+    echo "Adding source command to ~/.bashrc"
+    echo -e "\n# Load custom bash settings\nif [ -f ~/.mybashrc ]; then\n    $BASHRC_SNIPPET\nfi" >> ~/.bashrc
+fi
